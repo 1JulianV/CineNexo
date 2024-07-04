a4186f18ca3f9573c974f34c0f63d38742a2f5e0
Julian Villegas <140673778+1JulianV@users.noreply.github.com>
2024-07-04 18:26:56 -0300

    commit

--- back/src/middlewares/formulario.js
+++ back/src/middlewares/formulario.js
@@ -5,21 +5,6 @@ function validateMovies (req, res, next){
         message: "Faltan campos obligatorios"
        })
        }
-       if(year < 1900 || year > 2030){
-        return res.status(500).json({
-            message: "Ingrese un año correcto"
-           })
-       }
-       if(genre.lenght > 4){
-        return res.status(500).json({
-            message: "Ingrese maximo 3 géneros"
-           })
-       }
-       if(rate < 0 || year > 10){
-        return res.status(500).json({
-            message: "Ingrese maximo 3 géneros"
-           })    
-       }
        next()
     }
 
